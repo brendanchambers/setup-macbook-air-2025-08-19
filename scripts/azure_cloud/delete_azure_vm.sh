45a9bbfc7c16e70365c1d623fa984715baaa9d1c
brendan chambers <brendanchambers@gmail.com>
2025-09-05 15:42:03 -0400

    clean up other resources on deletE

--- scripts/azure_cloud/delete_azure_vm.sh
+++ scripts/azure_cloud/delete_azure_vm.sh
@@ -6,3 +6,28 @@ echo $MY_RESOURCE_GROUP_NAME
 echo $MY_VM_NAME
 
 az vm delete -g $MY_RESOURCE_GROUP_NAME -n $MY_VM_NAME  --force-deletion no --yes
+
+
+# Hmmm, this didn't delete the network security group or public ip address
+
+
+echo "deleting network security group..."
+echo "${MY_VM_NAME}NSG"
+
+az network nsg delete \
+    --resource-group $MY_RESOURCE_GROUP_NAME \
+    --name "${MY_VM_NAME}" \
+    --subscription $MY_SUBSCRIPTION
+
+
+# todo probably don't want a public ip anyway
+echo "deleting public ip address..."
+echo "${MY_VM_NAME}PublicIP"
+
+az network public-ip delete \
+    --resource-group $MY_RESOURCE_GROUP_NAME \
+     --name "${MY_VM_NAME}" \
+     --subscription $MY_SUBSCRIPTION
+
+
+
